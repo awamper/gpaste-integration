781cfa028329e302b859487480f3af7724646979
awamper <awamper@gmail.com>
2014-02-19 14:12:14 +0800

    support multimonitor, fixes #1

--- gpaste_integration.js
+++ gpaste_integration.js
@@ -458,18 +458,21 @@ const GPasteIntegration = new Lang.Class({
         let height_percents = Utils.SETTINGS.get_int(
             PrefsKeys.HEIGHT_PERCENTS_KEY
         );
-        let primary = Main.layoutManager.primaryMonitor;
-        let available_height = primary.height - Main.panel.actor.height;
-        let my_width = primary.width / 100 * width_percents;
+
+        let monitor = Main.layoutManager.currentMonitor;
+        let is_primary = monitor.index === Main.layoutManager.primaryIndex;
+
+        let available_height = monitor.height;
+        if(is_primary) available_height -= Main.panel.actor.height;
+
+        let my_width = monitor.width / 100 * width_percents;
         let my_height = available_height / 100 * height_percents;
 
-        this._hidden_y = primary.y - my_height;
-        this._target_y =
-            this._hidden_y
-            + my_height
-            + Main.layoutManager.panelBox.height;
+        this._hidden_y = monitor.y - my_height;
+        this._target_y = this._hidden_y + my_height;
+        if(is_primary) this._target_y += Main.panel.actor.height;
 
-        this.actor.x = primary.width - my_width;
+        this.actor.x = (monitor.width + monitor.x) - my_width;
         this.actor.y = this._hidden_y;
         this.actor.width = my_width;
         this.actor.height = my_height;
